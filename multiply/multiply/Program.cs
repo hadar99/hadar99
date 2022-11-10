49e68a602e9b9be0731545731d0dec518d9b78fb
hadar99 <117916004+hadar99@users.noreply.github.com>
2022-11-10 15:52:14 +0200

    Add files via upload

---
+++ multiply/multiply/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 0;
+int b = 0;
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+Console.WriteLine(a*b);
